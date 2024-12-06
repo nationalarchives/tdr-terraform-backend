aa79a2e3d811ceaf107a332fa6a8bf10dd99c96a
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-12-06 15:09:19 +0000

    jenkins bucket should be removed

--- modules/state/s3.tf
+++ modules/state/s3.tf
@@ -41,50 +41,6 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "tdr_terraform_state_jenkins" {
-  bucket = "tdr-terraform-state-jenkins"
-
-  tags = merge(
-    var.common_tags,
-    tomap(
-      { "Name" = "TDR Jekins Terraform State" }
-    )
-  )
-}
-
-resource "aws_s3_bucket_acl" "tdr_terraform_state_jenkins_acl" {
-  bucket = aws_s3_bucket.tdr_terraform_state_jenkins.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "tdr_terraform_state_jenkins_encryption" {
-  bucket = aws_s3_bucket.tdr_terraform_state_jenkins.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "tdr_terraform_state_jenkins_versioning" {
-  bucket = aws_s3_bucket.tdr_terraform_state_jenkins.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-resource "aws_s3_bucket_public_access_block" "public_access_block_jenkins" {
-  bucket = aws_s3_bucket.tdr_terraform_state_jenkins.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket" "tdr_terraform_state_grafana" {
   bucket = "tdr-terraform-state-grafana"
 
